06a3bab59d29af501d453c3a9890e515f4d26424
Arslan <rslnkrmt2552@gmail.com>
2019-05-05 13:03:52 +0300

    changed some paths

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import PrivateRoute from './components/private-route/PrivateRoute';
-import Dashboard from './components/dashboard/Dashboard';
+import Dashboard from './components/mainapp/Dashboard';
 
 import store from './store';
 
