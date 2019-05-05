9021c60c44b6551b0fc201b050074f8bb8df3ee0
Arslan <rslnkrmt2552@gmail.com>
2019-05-05 10:59:16 +0300

    Added Redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './App.css';
 
 import Navbar from './components/layout/Navbar';
@@ -7,17 +8,21 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+import store from './store';
+
 class App extends Component {
     render() {
         return (
-            <Router>
-                <div className='App'>
-                    <Navbar />
-                    <Route exact path='/' component={Landing} />
-                    <Route exact path='/register' component={Register} />
-                    <Route exact path='/login' component={Login} />
-                </div>
-            </Router>
+            <Provider>
+                <Router>
+                    <div className='App'>
+                        <Navbar />
+                        <Route exact path='/' component={Landing} />
+                        <Route exact path='/register' component={Register} />
+                        <Route exact path='/login' component={Login} />
+                    </div>
+                </Router>
+            </Provider>
         );
     }
 }
---
+++ client/src/actions/authActions.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+
+import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from './types';
+
+export const registerUser = (userData, history) => dispatch => {
+    axios
+        .post('/api/users/register', userData)
+        .then(res => history.push('/login'))
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data,
+            })
+        );
+};
+
+export const loginUser = userData => dispatch => {
+    axios
+        .post('/api/users/login', userData)
+        .then(res => {
+            // Saving to the local storage
+            const { token } = res.data;
+            localStorage.setItem('jwtToken', token);
+
+            // Set token to Authorization header
+            setAuthToken(token);
+
+            const decoded = jwt_decode(token);
+            dispatch(setCurrentUser(decoded));
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data,
+            });
+        });
+};
+
+export const setCurrentUser = decodedToken => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decodedToken,
+    };
+};
+
+export const setUserLoading = () => {
+    return {
+        type: USER_LOADING,
+    };
+};
+
+export const logoutUser = () => dispatch => {
+    localStorage.removeItem('jwtToken');
+    setAuthToken(false);
+    dispatch(setCurrentUser());
+};
