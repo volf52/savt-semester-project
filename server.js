ad611116dc3e45e03beb3f4b4f712d4886a49896
Arslan <rslnkrmt2552@gmail.com>
2019-05-06 18:52:09 +0300

    made some changed for deployment

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use('/api/ships', ships);
 app.use('/api/routing', routing);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
